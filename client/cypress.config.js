b6b9b98dca45365ac9da4bc6a72335b745abf1bc
mcaiati2 <93667565+mcaiati2@users.noreply.github.com>
2024-11-22 13:50:54 -0500

    edited action yaml

--- client/cypress.config.js
+++ client/cypress.config.js
@@ -1,11 +1,13 @@
 import { defineConfig } from "cypress";
 
+const is_prod = process.env.PORT;
+
 export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    baseUrl: "http://localhost:5173",
+    baseUrl: is_prod ? 'http://localhost:10000' : 'http://localhost:5173'
   },
 
   component: {
